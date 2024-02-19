c536f9e6e28e412e9b410b2ddc0e44a248b98646
arghaahit <argha.ahit@gmail.com>
2024-02-19 17:36:35 +0530

    changes

--- lib/taskpage/screen/upcommingtask.dart
+++ lib/taskpage/screen/upcommingtask.dart
@@ -1,7 +1,6 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:slider_button/slider_button.dart';
 
 class UpcommingTask extends StatefulWidget {
   const UpcommingTask({super.key});
@@ -12,10 +11,9 @@ class UpcommingTask extends StatefulWidget {
 
 class _UpcommingTaskState extends State<UpcommingTask> {
   int tabSwaip = 0;
-  List<Widget> tabs = const  [
+  List<Widget> tabs = const [
     TaskTab(),
     MyTask(),
-
   ];
   @override
   Widget build(BuildContext context) {
@@ -51,7 +49,8 @@ class _UpcommingTaskState extends State<UpcommingTask> {
                           },
                           child: Container(
                             decoration: BoxDecoration(
-                              color: tabSwaip == 0 ? Colors.white : Colors.black,
+                              color:
+                                  tabSwaip == 0 ? Colors.white : Colors.black,
                               borderRadius: BorderRadius.circular(10),
                             ),
                             child: Center(
@@ -78,7 +77,8 @@ class _UpcommingTaskState extends State<UpcommingTask> {
                           },
                           child: Container(
                             decoration: BoxDecoration(
-                              color: tabSwaip == 1 ? Colors.white : Colors.black,
+                              color:
+                                  tabSwaip == 1 ? Colors.white : Colors.black,
                               borderRadius: BorderRadius.circular(10),
                             ),
                             child: Center(
@@ -97,14 +97,37 @@ class _UpcommingTaskState extends State<UpcommingTask> {
                     ],
                   ),
                 ),
-                tabs[tabSwaip]
+                // if(tabSwaip == 0)...[
+                //   const TaskTab(),
+                // ],
+                // if(tabSwaip == 1)...[
+                //   const MyTask()
+                // ]
+                //            ListView.builder(
+                //   shrinkWrap: true,
+                //   physics: const NeverScrollableScrollPhysics(),
+                //   itemBuilder: (context, index){
+                //  return Padding(
+                //    padding: const EdgeInsets.all(8.0),
+                //    child: ListTile(
+                //     title: Text("Project Name", style: GoogleFonts.dmSerifDisplay(color: Colors.black),),
+                //     subtitle: Text("www.ahecounsling.com", style: GoogleFonts.dmSerifDisplay(color: Colors.grey),),
+                //    ),
+                //  );
+                // }),
+                ListView.builder(
+                    itemCount: 2,
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return TaskTab();
+                    })
               ],
             ),
           ),
         ));
   }
 }
-
 
 class TaskTab extends StatefulWidget {
   const TaskTab({super.key});
@@ -116,18 +139,84 @@ class TaskTab extends StatefulWidget {
 class _TaskTabState extends State<TaskTab> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemBuilder: (context, index){
-     return Padding(
-       padding: const EdgeInsets.all(8.0),
-       child: ListTile(
-        title: Text("Project Name", style: GoogleFonts.dmSerifDisplay(color: Colors.black),),
-        subtitle: Text("www.ahecounsling.com", style: GoogleFonts.dmSerifDisplay(color: Colors.grey),),
-       ),
-     );
-    });
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: () {
+          showModalBottomSheet<void>(
+            // context and builder are
+            // required properties in this widget
+            context: context,
+            builder: (BuildContext context) {
+              // we set up a container inside which
+              // we create center column and display text
+
+              // Returning SizedBox instead of a Container
+              return SizedBox(
+                height: 250,
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Project Name",
+                          style: GoogleFonts.dmSerifDisplay(
+                              color: Colors.black, fontSize: 22)),
+                      Text("Project Link",
+                          style: GoogleFonts.dmSerifDisplay(
+                              color: Colors.black, fontSize: 22)),
+                      Text("Project Location",
+                          style: GoogleFonts.dmSerifDisplay(
+                              color: Colors.black, fontSize: 22)),
+                              const SizedBox(
+                                height: 50,
+                              ),
+                      Center(
+                          child: SliderButton(
+                        action: () async {
+                          ///Do something here OnSlide
+                           Navigator.pop(context);
+                          return true;
+                        },
+                        label: Center(
+                          child: Text(
+                            "Slide to accept",
+                            style: GoogleFonts.dmSerifDisplay(color: Colors.black, fontSize: 18),
+                          ),
+                        ),
+                        icon: const Text(
+                          "👍",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 44,
+                          ),
+                        ),
+                      ))
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        child: Card(
+          child: ListTile(
+            title: Text(
+              "Project Name",
+              style: GoogleFonts.dmSerifDisplay(color: Colors.black),
+            ),
+            subtitle: Text(
+              "www.ahecounsling.com",
+              style: GoogleFonts.dmSerifDisplay(color: Colors.grey),
+            ),
+            trailing: const Icon(Icons.done_all),
+          ),
+        ),
+      ),
+    );
   }
 }
 
